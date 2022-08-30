0c514b29699d27c4364d4f135fcbce351c680b99
AyeshaAmdani661 <ayeshaamdani661@gmail.com>
2022-08-30 20:45:57 +0500

    add work

--- assets/js/index.js
+++ assets/js/index.js
@@ -4,33 +4,40 @@ const buttons = document.querySelectorAll('button');
 buttons.forEach((item) => {
     item.onclick = () => {
         if (item.id == 'clear') {
-            display.innerText = '';
+            display.value = '';
         }
         else if (item.id == 'backspace') {
-            let str = display.innerText.toString();
-            display.innerText = str.substr(0, str.length - 1);
+            let str = display.value.toString();
+            display.value = str.substr(0, str.length - 1);
         }
-        else if (display.innerHTML != '' && item.id == 'equal') {
-            display.innerText = eval(display.innerText);
+        else if (display.value != '' && item.id == 'equal') {
+            display.value = eval(display.value);
         }
-        else if (display.innerHTML == '' && item.id == 'equal') {
-            display.innerText = 'Empty!';
-            setTimeout(() => (display.innerText = ''), 2000);
+        else if (display.value == '' && item.id == 'equal') {
+            display.value = 'Empty!';
+            setTimeout(() => (display.value = ''), 2000);
         }
         else {
-            display.innerText += item.id;
+            display.value += item.id;
         }
     }
 });
 const themeToggleBtn = document.querySelector('.theme-toggler');
 const calculator = document.querySelector('.calculator');
-const toggleIcon = document.querySelector('.toggler-icon');
+const darkThemeOfTogglerIcon = document.querySelector('.dark-theme');
+const lightThemeOfTogglerIcon = document.querySelector('.light-theme');
 const body = document.querySelector('body');
-let isDark = true;
-
+// let isDark = true;
+darkThemeOfTogglerIcon.addEventListener('click', ()=>{
+    lightThemeOfTogglerIcon.style.display='block';
+    darkThemeOfTogglerIcon.style.display='none';
+    body.style.backgroundColor='#071115';
+})
+lightThemeOfTogglerIcon.addEventListener('click', ()=>{
+    darkThemeOfTogglerIcon.style.display='block';
+    lightThemeOfTogglerIcon.style.display='none';
+    body.style.backgroundColor='white';
+})
 themeToggleBtn.onclick = () => {
     calculator.classList.toggle('dark');
-    themeToggleBtn.classList.toggle('active');
-    themeToggleBtn.innerText='Dark'
-    isDark = !isDark;
 }
